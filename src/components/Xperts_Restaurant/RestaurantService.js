f2e535d1557ebbaa9af084ec6b73c6d4c4bbeebd
Kamthegabru <154924574+Kamthegabru@users.noreply.github.com>
2025-03-10 20:32:08 -0700

    Update RestaurantService.js

--- src/components/Xperts_Restaurant/RestaurantService.js
+++ src/components/Xperts_Restaurant/RestaurantService.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 
 export default function RestaurantService() {
 
